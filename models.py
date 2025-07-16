53b7fb745f7457ffa8a43be5832ff6cf992e84e8
Maciej Małutowski <S59440@student.ubb.edu.pl>
2025-07-16 23:44:59 +0200

    Initial commit

---
+++ models.py
@@ -0,0 +1,32 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+class Organizations(Base):
+    __tablename__ = "organizations"
+
+    # TODO
+
+class Channels(Base):
+    __tablename__ = "channels"
+
+    # TODO
+
+class Topics(Base):
+    __tablename__ = "topics"
+
+    # TODO
+
+class Notes(Base):
+    __tablename__ = "notes"
+
+    # TODO
+
+class Users(Base):
+    __tablename__ = "users"
+
+    # TODO
+
+class Organization_Users(Base):
+    __tablename__ = "organization_users"
+
+    # TODO
