aebcfc8806bd0e8e4254179edd3db3103b14fdf6
Maciej Małutowski <S59440@student.ubb.edu.pl>
2025-07-17 18:16:28 +0200

    Migrations

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ import os
 
 load_dotenv()
 
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine(os.getenv("DATABASE_URL"), echo=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
