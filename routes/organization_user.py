0a0730ac9e49a7ab852984bdaa0bb5c52b66709f
Maciej Małutowski <S59440@student.ubb.edu.pl>
2025-07-18 18:10:04 +0200

    Invite/remove/update role

--- routes/organization_user.py
+++ routes/organization_user.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException, status, Form
 from models import OrganizationUser, UserRoleEnum
 from database import db_dependency
 from schemas import ReadOrganizationUserResponse
+from routes.auth import user_dependency
 
 
 router = APIRouter(
@@ -59,3 +60,85 @@ async def delete_organization_user(
     db.delete(organization_user)
     db.commit()
     return {"message": f"User {user_id} removed from organization {organization_id} successfully"}
+
+@router.get("/{organization_id}/{user_id}/role")
+async def get_user_role(db: db_dependency, organization_id: int, user_id: int):
+    org_user = db.query(OrganizationUser).filter_by(organization_id=organization_id, user_id=user_id).first()
+    if not org_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found in the organization")
+    return {"role": org_user.role}
+
+@router.post("/invite")
+async def invite_user_to_organization(
+    user: user_dependency,
+    db: db_dependency,
+    organization_id: int,
+    invited_user_id: int,
+    role: UserRoleEnum = Form(UserRoleEnum.user)
+):
+    org_user = db.query(OrganizationUser).filter_by(
+        organization_id=organization_id,
+        user_id=user["user_id"]
+    ).first()
+    if not org_user or org_user.role != UserRoleEnum.owner:
+        raise HTTPException(status_code=403, detail="No permission to invite users to this organization")
+
+    # Dodaj nowego użytkownika do organizacji
+    existing_user = db.query(OrganizationUser).filter_by(
+        organization_id=organization_id,
+        user_id=invited_user_id
+    ).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="User already exists in the organization")
+
+    new_organization_user = OrganizationUser(
+        organization_id=organization_id,
+        user_id=invited_user_id,
+        role=role
+    )
+    db.add(new_organization_user)
+    db.commit()
+    db.refresh(new_organization_user)
+    return {"message": "User invited to organization successfully",}
+
+@router.delete("/RemoveUserFromOrganization")
+async def remove_user_from_organization(
+    user: user_dependency,
+    db: db_dependency,
+    organization_id: int,
+    user_id: int
+):
+    org_user = db.query(OrganizationUser).filter_by(
+        organization_id=organization_id,
+        user_id=user["user_id"]
+    ).first()
+    if not org_user or org_user.role != UserRoleEnum.owner:
+        raise HTTPException(status_code=403, detail="No permission to remove users from this organization")
+
+    organization_user = db.query(OrganizationUser).filter_by(
+        organization_id=organization_id,
+        user_id=user_id
+    ).first()
+    if not organization_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found in the organization")
+
+    db.delete(organization_user)
+    db.commit()
+    return {"message": f"User {user_id} removed from organization {organization_id} successfully"}
+
+@router.put("/{organization_id}/{user_id}/role")
+async def update_user_role(
+    db: db_dependency,
+    organization_id: int,
+    user_id: int,
+    role: UserRoleEnum = Form(UserRoleEnum.user)
+):
+    organization_user = db.query(OrganizationUser).filter_by(
+        organization_id=organization_id, user_id=user_id
+    ).first()
+    if not organization_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found in the organization")
+
+    organization_user.role = role
+    db.commit()
+    return {"message": f"User {user_id} role updated to {role} in organization {organization_id}"}
