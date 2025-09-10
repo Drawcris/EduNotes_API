e2bf3876bed15467d02fa2a5321f1f8e35b233f7
Maciej Małutowski <127963223+Drawcris@users.noreply.github.com>
2025-09-10 13:53:48 +0200

    Update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -9,6 +9,10 @@ from alembic import context
 # access to the values within the .ini file in use.
 config = context.config
 
+DATABASE_URL = os.environ.get("DATABASE_URL")
+if DATABASE_URL:
+    config.set_main_option("sqlalchemy.url", DATABASE_URL)
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
